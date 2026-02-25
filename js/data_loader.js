c69e90da2172c8ece3c1dac3da9732a6ce6fffd7
lethanhlongdvn <lethanhlong.dvn@gmail.com>
2026-02-26 06:36:55 +0700

    Create data_loader.js

---
+++ js/data_loader.js
@@ -0,0 +1,12 @@
+import { mathData } from './data/math/index.js';
+import { vietnameseData } from './data/vietnamese/index.js';
+
+export const lessons = {
+    math: mathData,
+    vietnamese: vietnameseData,
+    science: [],
+    history: [],
+    ethics: [],
+    experiential: [],
+    english: []
+};
