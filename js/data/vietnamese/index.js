b0f81e4092e560d03864ff21e8c05da29666afd2
lethanhlongdvn <lethanhlong.dvn@gmail.com>
2026-02-26 06:36:52 +0700

    Create index.js

---
+++ js/data/vietnamese/index.js
@@ -0,0 +1,11 @@
+import { lesson191 } from './191.js';
+import { lesson192 } from './192.js';
+import { lesson201 } from './201.js';
+import { lesson202 } from './202.js';
+
+export const vietnameseData = [
+    lesson191,
+    lesson192,
+    lesson201,
+    lesson202
+];
