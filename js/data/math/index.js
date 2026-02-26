ba2e3d21ccb139486b630d22b1df8bbf1ba6babc
lethanhlongdvn <lethanhlong.dvn@gmail.com>
2026-02-26 22:39:04 +0700

    Update index.js

--- js/data/math/index.js
+++ js/data/math/index.js
@@ -1,7 +1,9 @@
 import { lesson111 } from './111.js';
+import { lesson112 } from './112.js';
 import { lessonTemplate } from './template.js';
 
 export const mathData = [
     lesson111,
+    lesson112,
     lessonTemplate
 ];
