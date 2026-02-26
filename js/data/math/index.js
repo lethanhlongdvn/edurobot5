f994c0a867167f4ccf3b4b3b90170baca14b9ef8
lethanhlongdvn <lethanhlong.dvn@gmail.com>
2026-02-27 05:38:36 +0700

    Update index.js

--- js/data/math/index.js
+++ js/data/math/index.js
@@ -1,9 +1,13 @@
 import { lesson111 } from './111.js';
 import { lesson112 } from './112.js';
+import { lesson113 } from './113.js';
+import { lesson114 } from './114.js';
 import { lessonTemplate } from './template.js';
 
 export const mathData = [
     lesson111,
     lesson112,
+    lesson113,
+    lesson114,
     lessonTemplate
 ];
