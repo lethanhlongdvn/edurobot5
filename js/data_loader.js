0ec86f60a44b012853e644139e12be1e2cea580a
lethanhlongdvn <lethanhlong.dvn@gmail.com>
2026-03-03 21:51:25 +0700

    Update data_loader.js

--- js/data_loader.js
+++ js/data_loader.js
@@ -1,7 +1,6 @@
 import { scienceData } from './data/science/index.js';
 import { mathData } from './data/math/index.js';
 import { vietnameseData } from './data/vietnamese/index.js';
-import { scienceData } from './data/science/index.js';
 
 export const lessons = {
     math: mathData,
